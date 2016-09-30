398a1c27c05a438424aea2237adea1cae171f4dd
Tiffany Glover <tif.sheline@gmail.com>
2016-09-30 15:19:00 -0700

    Add server.js

---
+++ server.js
@@ -0,0 +1,37 @@
+var
+  express = require('express'),
+  app = express(),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  shirtRoutes = require('./routes/shirtRoutes.js')
+
+  // connection to mongo db:
+  mongoose.connect('mongodb://localhost/shirt', function(err){
+    if(err) {
+      console.log("Problem connecting to Mongo.")
+    } else {
+      console.log("Connected to Mongo.")
+    };
+  });
+
+  function ourLogger(req, res, next){
+    console.log(req.method, req.url);
+    next()
+  }
+
+  function ourMiddleware(req, res, next){
+    console.log("Passing through our middleware...and continuing!");
+    next();
+  }
+
+  app.use(ourLogger);
+  app.use(ourMiddleware);
+  app.use(bodyParser.json());
+
+  // connects shirtRoutes.js to server.js
+  app.use('/', shirtRoutes);
+
+  app.listen(3000, function(){
+    console.log("Server is running on port 3000.")
+  })
